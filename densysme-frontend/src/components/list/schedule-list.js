6474ea2511e051b6b293c65f981cae16a39aea09
Ulpan Beken <u.beken@typi.team>
2022-12-03 22:21:07 +0600

    choose appointmentDay in schedule-list.js

--- densysme-frontend/src/components/list/schedule-list.js
+++ densysme-frontend/src/components/list/schedule-list.js
@@ -23,47 +23,50 @@ import CardMedia from '@mui/material/CardMedia';
 export const ScheduleList = () => {
     const[query, setQuery] = useState("")
     const [schedules, setSchedule] = useState([
-    {
-        "name": "static doctor",
-        "specialization": "Surg",
-        "appointment_day": "Wednesday",
-        "available_time": "13:00"
-    },
-    {
-        "name": "static doctor 2",
-        "specialization": "Eye",
-        "appointment_day": "Wednesday",
-        "available_time": "12:00"
+        {
+            "name": "static doctor",
+            "specialization": "Surg",
+        },
+        {
+            "name": "static doctor 2",
+            "specialization": "Eye",
+        }
+    ])
+    const appointmentDay = {
+        value: '0'
     }
-    ])   
+
+    const onChangeDay = (e) => {
+        appointmentDay.value = e.target.value
+    }
     const [page, setPage] = useState(0)
     const [rowsPerPage, setRowsPerPage] = useState(10)
     const [openModal, setOpenModal] = useState(false)
-    const [activeScheduleId, setActiveScheduleId] = useState("")   
-    const [doctors, setDoctors] = useState([])  
+    const [activeScheduleId, setActiveScheduleId] = useState("")
+    const [doctors, setDoctors] = useState([])
     const [loading,setloading] = useState()
     const [error,setError] = useState()
-    
+
     const fetchDoctors = async () => {
         // !! UNCOMMENT!!
         const doctors = await DoctorApi.getAllDoctors()
         setDoctors(doctors)
     }
 
-     useEffect(() => {
-         fetchDoctors().catch(console.error)
-         
-     }, [])
+    useEffect(() => {
+        fetchDoctors().catch(console.error)
+
+    }, [])
 
 
 
-    // const fetchSchedule = async () => {  
+    // const fetchSchedule = async () => {
     //     const schedules = await ScheduleApi.getAllTimeSlots()
     //     setSchedule(schedules)
     // }
 
 
-    // useEffect(() => {    
+    // useEffect(() => {
     //   fetchSchedule().catch(console.error)
     // }, [])
 
@@ -76,24 +79,24 @@ export const ScheduleList = () => {
         //     })
         // }
     }
-    
-    
-    
+
+
+
     return(
         <>
             <div className={`${classes.pageHeader} ${classes.mb2}`}>
                 <Typography variant="h5"> Schedule appointment with Doctor</Typography>
                 <input type = "text"
-                placeholder="Search"
-                className="search"
-                onChange={(e)=> setQuery(e.target.value.toLowerCase())}/>
+                       placeholder="Search"
+                       className="search"
+                       onChange={(e)=> setQuery(e.target.value.toLowerCase())}/>
 
             </div>
-            
+
             {schedules.length > 0 ? (
-                
+
                 <>
-                
+
 
                     <div className={classes.tableContainer}>
                         <TableContainer component={Paper}>
@@ -102,8 +105,7 @@ export const ScheduleList = () => {
                                     <TableRow>
                                         <TableCell>Name</TableCell>
                                         <TableCell align="right">Specialization</TableCell>
-                                        <TableCell align="right">Appointment day</TableCell>
-                                        <TableCell align="right">Available time</TableCell>
+                                        <TableCell align="right">Appointment Day</TableCell>
                                         <TableCell>Action</TableCell>
                                     </TableRow>
                                 </TableHead>
@@ -117,19 +119,27 @@ export const ScheduleList = () => {
                                                 {schedule.name}
                                             </TableCell>
                                             <TableCell align="right">{schedule.specialization}</TableCell>
-                                            <TableCell align="right">{schedule.appointment_day}</TableCell>
-                                            <TableCell align="right">{schedule.available_time}</TableCell>
+                                            <TableCell align="right">
+                                                <select value={appointmentDay} onChange={onChangeDay}>
+                                                    <option value="0">Monday</option>
+                                                    <option value="1">Tuesday</option>
+                                                    <option value="2">Wednesday</option>
+                                                    <option value="3">Thursday</option>
+                                                    <option value="4">Friday</option>
+                                                </select>
+
+                                            </TableCell>
                                             <TableCell>
                                                 <div className={classes.actionsContainer}>
-                                                 <Button
+                                                    <Button
                                                         variant="contained"
                                                         component={RouterLink}
                                                         size="small"
                                                         to={`/admin/confirmation`}
-                                                        
+
                                                     >
                                                         Get Appointment
-                                                    </Button> 
+                                                    </Button>
                                                 </div>
                                             </TableCell>
                                             <TableCell>
@@ -153,37 +163,37 @@ export const ScheduleList = () => {
                             onPageChange={(e, newPage) => setPage(newPage)}
                         />
 
-                    {loading ? (<h1>Details about doctors</h1>) :error ? (<h1>Something went wrong</h1>) : (doctors.map(doctor=>{
-                       return (
+                        {loading ? (<h1>Details about doctors</h1>) :error ? (<h1>Something went wrong</h1>) : (doctors.map(doctor=>{
+                            return (
 
-                            <div className="wrapper">
-                                <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-                            <Card className="card" sx={{ maxWidth: 345 }}>
-                              <div className={classes.imageContainer}>
-                               <img
-                                width="100%"
-                                height="100%"
-                                className={classes.imageContainer}
-                                src={doctor.photo}/>
+                                <div className="wrapper">
+                                    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+                                        <Card className="card" sx={{ maxWidth: 345 }}>
+                                            <div className={classes.imageContainer}>
+                                                <img
+                                                    width="100%"
+                                                    height="100%"
+                                                    className={classes.imageContainer}
+                                                    src={doctor.photo}/>
+                                            </div>
+                                            <CardContent>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    {doctor.name}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    <b>  <p > Specialization:    {doctor.name}</p>
+                                                        <p> Contact Number:    {doctor.contactNum}</p>
+                                                        <p> Schedule:    {doctor.schedule}</p>
+                                                        <p> Price:    {doctor.price}</p>
+                                                        <p> Rating:     {doctor.rating}</p>   </b>
+                                                </Typography>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
                                 </div>
-                              <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                  {doctor.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                <b>  <p > Specialization:    {doctor.name}</p>
-                                  <p> Contact Number:    {doctor.contactNum}</p>
-                                  <p> Schedule:    {doctor.schedule}</p>
-                                  <p> Price:    {doctor.price}</p>
-                                  <p> Rating:     {doctor.rating}</p>   </b>
-                                </Typography>
-                              </CardContent>
-                            </Card>
-                            </div>
-                        </div>
-                          );  
-                    })) } 
-                    </div>   
+                            );
+                        })) }
+                    </div>
                 </>
             ) : (
                 <Typography variant="h5">No appointment exists!</Typography>
