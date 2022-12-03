edc97d0bfb57e152296fa1aac104c7c436d662e8
Ulpan Beken <u.beken@typi.team>
2022-12-03 22:21:54 +0600

    choose timeslot in confirmation.js

--- densysme-frontend/src/components/details/confirmation.js
+++ densysme-frontend/src/components/details/confirmation.js
@@ -2,13 +2,21 @@ import "./confirmation.css";
 import FormInput from "./component/FormInput";
 import { useState } from "react";
 
+
 export const Confirmation = () => {
   const [values, setValues] = useState({
     name: "",
     surname: "",
     email: "",
-    phonenumber: ""
+    phonenumber: "",
+    timeslot: ""
   });
+
+
+  const timeslot = {
+    value: '12:00'
+  }
+
 
   const inputs = [
     {
@@ -17,7 +25,7 @@ export const Confirmation = () => {
       type: "text",
       placeholder: "Name",
       errorMessage:
-        "Name should be 3-16 characters and shouldn't include any special character!",
+          "Name should be 3-16 characters and shouldn't include any special character!",
       label: "Enter your name",
       pattern: "^[A-Za-z0-9]{3,16}$",
       required: true
@@ -28,7 +36,7 @@ export const Confirmation = () => {
       type: "text",
       placeholder: "Surname",
       errorMessage:
-        "Surname should be 3-16 characters and shouldn't include any special character!",
+          "Surname should be 3-16 characters and shouldn't include any special character!",
       label: "Enter your surname",
       pattern: "^[A-Za-z0-9]{3,16}$",
       required: true
@@ -62,21 +70,36 @@ export const Confirmation = () => {
     setValues({ ...values, [e.target.name]: e.target.value });
   };
 
+  const onChangeForTimeSlot = (e) => {
+    timeslot.value = e.target.value
+  }
+
   console.log(values);
   return (
-    <div className="app">
-    <form on Submit={handleSubmit}>
-        <h1>Confirmation</h1>
-        {inputs.map((input) => (
-          <FormInput
-            key={input.id}
-            {...input}
-            value={values[input.name]}
-            onChange={onChange}
-          />
-        ))}
-        <button> Confirm </button>
-      </form>
-    </div>
+      <div className="app">
+        <form on Submit={handleSubmit}>
+          <h1>Confirmation</h1>
+          {inputs.map((input) => (
+              <FormInput
+                  key={input.id}
+                  {...input}
+                  value={values[input.name]}
+                  onChange={onChange}
+              />
+          ))}
+          <label>
+            Pick available time slot:
+            <select value={timeslot} onChange={onChangeForTimeSlot}>
+              <option value="0">09:00</option>
+              <option value="1">10:00</option>
+              <option value="2">11:00</option>
+              <option value="3">12:00</option>
+              <option value="4">13:00</option>6
+            </select>
+          </label>
+          <button> Confirm </button>
+        </form>
+
+      </div>
   );
 }
